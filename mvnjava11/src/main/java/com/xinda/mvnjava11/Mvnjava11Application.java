c7a78370ede21e6d65039582de3667b5e0c8d11d
Chunfeng <cliu3@rocketsoftware.com>
2022-08-08 16:32:51 +0800

    feat: Add maven java 11 demo

---
+++ mvnjava11/src/main/java/com/xinda/mvnjava11/Mvnjava11Application.java
@@ -0,0 +1,14 @@
+package com.xinda.mvnjava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvnjava11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvnjava11Application.class, args);
+    System.out.println("Hello World!");
+	}
+
+}
---
+++ mvnjava11/src/test/java/com/xinda/mvnjava11/Mvnjava11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinda.mvnjava11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvnjava11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
